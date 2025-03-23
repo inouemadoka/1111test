80ea2a2fb6c330514246b64ff9d1d9e7a31f88fb
pipopa123 <inouemadoka.yuan@gmail.com>
2025-03-23 17:19:58 +0900

    add header_body

--- .github/script/review.php
+++ .github/script/review.php
@@ -19,3 +19,40 @@ $apiKey = getenv('Cloud_API_secret');
 if(empty($apiKey)){
     echo "キーが取得できませんでした";
 }
+
+
+$header =[
+
+    "x-api-key:" .$apiKey,
+    "anthropic-version: 2023-06-01",
+    "content-type: application/json"
+];
+
+$body = [
+    "model"=> "claude-3-7-sonnet-20250219",
+    "max_tokens" => 1024,
+    "messages"=> [
+        [
+            "role" => "user", 
+            "content" => "Hello, world"
+        ]
+    ]
+];
+
+$ch = curl_init('https://api.anthropic.com/v1/messages');//endpoint
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body));
+
+$reponse = curl_exec($ch);
+$httpStatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+$responseData =json_decode($response);
+
+if($httpStatus === 200){
+    echo "成功" . json_encode($responseData). PHP_EOL;
+}else{
+    echo"ステータス:" . $httpStatus . " : ". json_encode($responseData). PHP_EOL;
+}
